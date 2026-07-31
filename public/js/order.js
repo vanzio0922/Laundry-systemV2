0dff24e45f6fbae309d1a4a052efdb7f50dd285b
vanzio0922 <vanzio0922@gmail.com>
2026-08-01 02:45:27 +0700

    Create order.js to manage order-related functions

---
+++ public/js/order.js
@@ -0,0 +1,7 @@
+// order.js
+import { renderOrderList } from './order/list.js';
+import { renderOrderDetail } from './order/detail.js';
+import { handleSaveOrder } from './order/save.js';
+import { handleUpload } from './order/upload.js';
+
+export { renderOrderList, renderOrderDetail, handleSaveOrder, handleUpload };
